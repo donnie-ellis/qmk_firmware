4d247c7ad56ea0ac34b8aac2370980285ebcd540
Donnie Ellis <donnie@dmellis.com>
2026-09-15 11:23:30 -0400

    fix colors

--- keyboards/keychron/q10/ansi_encoder/keymaps/donnie/keymap.c
+++ keyboards/keychron/q10/ansi_encoder/keymaps/donnie/keymap.c
@@ -73,13 +73,21 @@ bool rgb_matrix_indicators_user(void) {
             if (win_mode) {
                 rgb_matrix_set_color_all(RGB_BLUE);
             } else if (mac_profile2) {
-                rgb_matrix_set_color_all(auto_shift_active ? RGB_RED : RGB_WHITE);
+                if (auto_shift_active) {
+                    rgb_matrix_set_color_all(RGB_RED);
+                } else {
+                    rgb_matrix_set_color_all(RGB_WHITE);
+                }
             } else {
                 rgb_matrix_set_color_all(RGB_PURPLE);
             }
             break;
         case MAC_FN:
-            rgb_matrix_indicate_fn_keys(MAC_FN, mac_profile2 ? RGB_PINK : RGB_YELLOW);
+            if (mac_profile2) {
+                rgb_matrix_indicate_fn_keys(MAC_FN, RGB_PINK);
+            } else {
+                rgb_matrix_indicate_fn_keys(MAC_FN, RGB_YELLOW);
+            }
             break;
         case WIN_FN:
             rgb_matrix_indicate_fn_keys(WIN_FN, RGB_ORANGE);
